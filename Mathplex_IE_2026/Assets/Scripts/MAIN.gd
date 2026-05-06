3b3d3d0641fe6dd914a0614530df97cb7f91f7fa
Miki <anrb853@gmail.com>
2026-05-07 01:12:00 +0300

    Mathplex v1.0
    Demo version of the Menu layout - 07.05.2026

---
+++ Mathplex_IE_2026/Assets/Scripts/MAIN.gd
@@ -0,0 +1,51 @@
+extends MarginContainer
+
+@onready var start_screen = $Start
+@onready var menu_screen = $Menu
+@onready var options_screen = $Options
+@onready var profile_screen = $Profile
+@onready var levels_screen = $Levels
+
+@export var experience = 85
+
+
+func toggle_visibility(object):
+	object.visible = !object.visible
+	
+func menu_options():
+	toggle_visibility(options_screen)
+	toggle_visibility(menu_screen)
+
+
+func _on_menu_button_pressed() -> void:
+	toggle_visibility(start_screen)
+	toggle_visibility(menu_screen)
+
+
+func escape() -> void:
+	get_tree().quit()
+
+func _ready() -> void:
+	start_screen.visible = true
+	menu_screen.visible = false
+	options_screen.visible = false
+	profile_screen.visible = false
+	levels_screen.visible = false
+
+
+func back_button() -> void:
+	start_screen.visible = false
+	menu_screen.visible = true
+	options_screen.visible = false
+	profile_screen.visible = false
+	levels_screen.visible = false
+
+
+func menu_profile() -> void:
+	toggle_visibility(profile_screen)
+	toggle_visibility(menu_screen)
+
+
+func menu_levels() -> void:
+	toggle_visibility(levels_screen)
+	toggle_visibility(menu_screen)
